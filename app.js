1afba2471fa2157024e7cda3a61eb0de7a009547
Kyle Devine <devinenoise@gmail.com>
2021-02-17 13:12:40 -0800

    shift check

---
+++ app.js
@@ -0,0 +1,25 @@
+const checkboxes = document.querySelectorAll('.inbox [type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+  // Check if they had the shift key down AND check that the box is being checked
+  let inBetween = false;
+  // hold down shift and check
+  if (e.shiftKey && this.checked) {
+    // loop over every checkbox
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      // if it's between the two start and end boxes they should also be checked
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
